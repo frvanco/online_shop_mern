b5ff9c203ce2b5fd3ba29d43654c491acd3979c1
frvanco <antoinevancompernolle@gmail.com>
2021-09-01 17:55:04 +0200

    start to add uploadproduct

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function UploadProductPage() {
+    return(
+        <div style={{maxWidth:'700px', margin:'2rm auto'}}>
+            <div style={{ textAlign:'center', marginBottom:'2rem'}}>
+          
+          
+          </div>
+          
+          <form onSubmit>
+                {/* DropZone */}
+        <br/>
+        <br/>
+        <label>Title</label>
+        <input 
+        onChange
+        value
+        />
+        <br/>
+        <br/>
+        <label>Description</label>
+        <textarea
+        onChange
+        value
+        
+        />
+
+          </form>
+        </div> 
+    )
+}
+
+export default UploadProductPage
