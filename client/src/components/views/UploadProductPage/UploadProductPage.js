aa178cfdbdeef6a0c268a8354d1f4f8b78427c81
frvanco <antoinevancompernolle@gmail.com>
2021-09-02 08:38:51 +0200

    start the upload product page

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,34 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function UploadProductPage() {
-    return(
-        <div style={{maxWidth:'700px', margin:'2rm auto'}}>
-            <div style={{ textAlign:'center', marginBottom:'2rem'}}>
-          
-          
-          </div>
-          
-          <form onSubmit>
-                {/* DropZone */}
-        <br/>
-        <br/>
-        <label>Title</label>
-        <input 
-        onChange
-        value
-        />
-        <br/>
-        <br/>
-        <label>Description</label>
-        <textarea
-        onChange
-        value
-        
-        />
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rm auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}></div>
 
-          </form>
-        </div> 
-    )
+      <form onSubmit>
+        {/* DropZone */}
+        <br />
+        <br />
+        <label>Title</label>
+        <input onChange value />
+        <br />
+        <br />
+        <label>Description</label>
+        <textarea onChange value />
+      </form>
+    </div>
+  );
 }
 
-export default UploadProductPage
+export default UploadProductPage;
